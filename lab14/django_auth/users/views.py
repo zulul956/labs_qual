aee4749dc7689c4567a60212a24ebc5033ffe0ea
zulul956 <zazulya-24@mail.ru>
2024-06-03 13:53:14 +0500

    lab14

---
+++ lab14/django_auth/users/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
+
+def home(request):
+    return render(request, "users/home.html")
+
+class SignUp(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "registration/signup.html"
